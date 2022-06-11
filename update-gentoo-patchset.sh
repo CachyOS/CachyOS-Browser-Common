7435cb9c7dfc1526f717279a9b99f229f89e9920
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2022-06-11 22:51:26 +0400

    🔄 Sync with upstream

---
+++ update-gentoo-patchset.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -e
+
+cd "`dirname "$0"`"
+
+UPPER="$PWD"
+
+FIREFOX_PATCHSET="firefox-101-patches-04j.tar.xz"
+GENTOO_PATH="$UPPER/patches/gentoo"
+
+curl -o $UPPER/$FIREFOX_PATCHSET https://dev.gentoo.org/~{juippis,polynomial-c,whissi,slashbeast}/mozilla/patchsets/${FIREFOX_PATCHSET}
+tar -xf $UPPER/$FIREFOX_PATCHSET
+
+rm -f $GENTOO_PATH/*
+mv firefox-patches/* $GENTOO_PATH/
+rm -rf firefox-patches
+
+rm -f $UPPER/$FIREFOX_PATCHSET
