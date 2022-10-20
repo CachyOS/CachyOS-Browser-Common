2f54b90ee78ff243e98fb318ad2c331a544772a2
Peter Jung <admin@ptr1337.dev>
2022-10-20 23:39:33 +0200

    Updates patches
    Signed-off-by: Peter Jung <admin@ptr1337.dev>

--- update-gentoo-patchset.sh
+++ update-gentoo-patchset.sh
@@ -6,7 +6,7 @@ cd "`dirname "$0"`"
 
 UPPER="$PWD"
 
-FIREFOX_PATCHSET="firefox-105-patches-05j.tar.xz"
+FIREFOX_PATCHSET="firefox-106-patches-01j.tar.xz"
 GENTOO_PATH="$UPPER/patches/gentoo"
 
 curl -o $UPPER/$FIREFOX_PATCHSET https://dev.gentoo.org/~{juippis,polynomial-c,whissi,slashbeast}/mozilla/patchsets/${FIREFOX_PATCHSET}
