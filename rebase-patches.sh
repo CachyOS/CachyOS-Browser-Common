94599cdea19ba19e17aa3239946e071c629d1f96
Vladislav Nepogodin <nepogodin.vlad@gmail.com>
2023-02-15 02:48:35 +0400

    WIP: Add script rebase script

---
+++ rebase-patches.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+set -e
+
+cd "`dirname "$0"`"
+
+UPPER="$PWD"
+PATCHSET_DIR="$UPPER/patches"
+BRANCH_TO_BUILD="master"
+
+function apply_diff_as_patch {
+    local _diff_file="$1"
+
+    # Convert the diff file into a Git patch file
+    git apply --3way "${_diff_file}" || echo "Failed '${_diff_file}'" && return
+
+    # Create a Git commit from the staged changes
+    git commit -m "Apply diff file"
+
+    # Update the diff file in-place
+    git diff HEAD^ > "$_diff_file"
+}
+
+function rebase_patch {
+    local _patch_file="$1"
+
+    # Rebase the local master branch against the remote repository
+    #git rebase "origin/${BRANCH_TO_BUILD}" --autostash
+    git reset --hard "origin/${BRANCH_TO_BUILD}"
+
+    # Apply the Git patch
+    if ! git am --3way "${_patch_file}" ; then
+        git am --abort &>/dev/null || true
+        apply_diff_as_patch "${_patch_file}" || true
+        return
+    fi
+
+    # If there are conflicts during the rebase, the following command
+    # will automatically resolve the conflicts using the version from the patch
+    git am --continue || true
+
+    # Update the Git patch file in-place
+    git format-patch HEAD^ --stdout > "$_patch_file"
+}
+
+[[ -d "gecko-dev" ]] || git clone -b "${BRANCH_TO_BUILD}" --depth 1 --single-branch "https://github.com/mozilla/gecko-dev.git" gecko-dev
+
+GENTOO_PATH="$PATCHSET_DIR/gentoo"
+cd "$UPPER/gecko-dev"
+git config commit.gpgsign false
+for entry in "$GENTOO_PATH/"*; do rebase_patch "$entry"; done;
